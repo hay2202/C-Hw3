9bde0b0596ea63053a0f345919f81c4d18b54ad4
user <hayasa229@gmail.com>
2020-12-28 20:13:14 +0200

    commit

--- txtfind.c
+++ txtfind.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define LINE 256
 #define WORD 30
 
 int get_line(char s[]){
     int num_of_char = 0;
-    char c ='a';
+    char c =getchar();
 
-    while(c != '\n' || num_of_char != LINE){
-        scanf("%c",&c);
-        strcat(s,&c);
+    while(c != '\n' && num_of_char != LINE){
+        strncat(s,&c,1);
         num_of_char++;
+        c =getchar();
     }
     return num_of_char;
 }
 
-int getword(char w[]){
+int get_word(char w[]){
     int num_of_char = 0;
-    char c ='a';
-
-    while(c != '\n' || c != '\t' ||c != ' ' || num_of_char != WORD){
-        scanf("%c",&c);
-        strcat(w,&c);
+    char c =getchar();
+    
+    while(c != '\n' && c != '\t' && c != ' ' && num_of_char != WORD){ 
+        strncat(w,&c,1);
         num_of_char++;
+        c =getchar();
     }
     return num_of_char;
 }
@@ -41,8 +42,8 @@ int similar (char *s, char *t, int n){
     int len_s = strlen(s);
     int len_t = strlen(t);
     int miss, i , j;
-
-    while(i<len_t || j<len_s){
+    
+    while(i<len_t && j<len_s){
         if(t[i] == s[j]){
             i++;
             j++;
@@ -60,34 +61,58 @@ int similar (char *s, char *t, int n){
 }
 
 void print_lines(char * str){
-    while(!EOF){
-        char line[LINE];
-        get_line(line);
+    char c =getchar();
+    int n;
+
+    while(c != EOF){
+       char *line = (char*) calloc(LINE,sizeof(char));
+       if (line == NULL)
+       {
+           printf("Error\n");
+           return;
+       }
+        line[0] = c;
+        n = get_line(line);
         if (substring(line, str))
         {
-            printf("%s",line);
-        }           
+            printf("%s\n",line);
+        } 
+        c =getchar();
+        if (line != NULL){
+            free(line);  
+        }       
     }
 }
 
 void print_similar_words(char * str){
-    while (!EOF)
+     char c =getchar();
+    while (c != EOF)
     {
-       char word[WORD];
-       getword(word);
-       if (similar(str,word,1)==1 || similar(str,word,0))
+       char *temp = (char*) calloc(WORD,sizeof(char));
+       if (temp == NULL)
        {
-           printf("%s",word);
+            printf("Error\n");
+            return;
        }
+       temp[0]=c;
+       get_word(temp);
+       if (similar(str,temp,1)==1 || similar(str,temp,0)==1)
+       {
+           printf("%s\n",temp);
+       }
+       if (temp != NULL){
+            free(temp);  
+        } 
+       c =getchar();
     } 
 }
 
 int main(){
-    char option[2];
-    char word[WORD];
+    char option[2] = {0};
+    char word[WORD] = {0};
 
-    getword(word);
-    getword(option);
+    get_word(word);
+    get_word(option);
 
     switch (option[0])
     {
